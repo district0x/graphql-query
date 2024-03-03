3acffd5cabbd8ac6d5b3b607d80f76acdafdecfc
Madis Nõmme <me@mad.is>
2024-03-03 18:11:09 +0100

    Use shadow-cljs & deps.edn for building & testing

--- test/graphql_query/runner.cljs
+++
@@ -1,9 +0,0 @@
-(ns graphql-query.runner
-  (:require [doo.runner :refer-macros [doo-tests]]
-            [graphql-query.core-test]
-            [graphql-query.spec-test]))
-
-(enable-console-print!)
-
-(doo-tests 'graphql-query.core-test
-           'graphql-query.spec-test)
